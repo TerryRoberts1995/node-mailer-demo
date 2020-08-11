d1cfa06d4384ed76576c3fc8649b44609f4f1515
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-11 11:28:28 -0500

    added functionality

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ app.use(express.json());
 
 const myOAuth2Client = new OAuth2(
     process.env.CLIENT_ID,
-    process.env, CLIENT_SECRET,
+    process.env.CLIENT_SECRET,
     'https://developers.google.com/oauthplayground'
 );
+
+myOAuth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+})
 
 
 const myAccessToken = myOAuth2Client.getAccessToken();
@@ -31,12 +35,27 @@ const transport = nodemailer.createTransport({
     }
 })
 
-myOAuth2Client.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
-})
-
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Root route , refer to docs for more info" });
+})
+
+app.post("/send-email", (req, res) => {
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.USER_EMAIL,
+        subject: "Ecommerce store contact form request.",
+        html: `<p> You have a message from the contact form: </p><p>${req.body.message}</p>`
+    }
+
+    transport.sendMail(mailOptions, (error, result) => {
+        if (error) {
+            res.json({ message: `${error}` })
+            transport.close()
+        } else {
+            transport.close()
+            res.json({ message: "Email sent" });
+        }
+    })
 })
 
 app.listen(PORT, () => {
